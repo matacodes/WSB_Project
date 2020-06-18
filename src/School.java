22762c3bd867d66bdc0995d70dcd9f608dbbb98d
matacodes <ma.matysek@gmail.com>
2020-06-18 20:59:58 +0200

    School class

--- src/School.java
+++ src/School.java
@@ -1,14 +1,30 @@
+import java.util.List;
+
 /**
  * 
  * @author matacodes on  18/06/2020
+ * 
+ * School have many teachers and students - using ArrayList.
  *
  */
 public class School {
 
-	private Teacher teacher;
-	private Student student;
+	private List<Teacher> teachers;
+	private List<Student> students;
 	private int totalMoneyEarned;
 	private int totalMoneySpend;
 	
+	/**
+	 * Created of new school object
+	 * @param teachers - list of teachers in school
+	 * @param students - list of students in school
+	 */
+	
+	public School(List<Teacher> teachers, List<Student> students) {
+		this.teachers = teachers;
+		this.students = students;
+	}
+	
+
 
 }
